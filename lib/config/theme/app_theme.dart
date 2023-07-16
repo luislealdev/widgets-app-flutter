61e6207ecb7a9839224762a41d87d65ae87a9316
Luis Leal <luisrrleal@gmail.com>
2023-07-16 10:56:28 -0600

    Add theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const colors = <Color>[
+  Colors.red,
+  Colors.blue,
+  Colors.yellow,
+  Colors.green,
+  Colors.purple
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({required this.selectedColor})
+      : assert(selectedColor >= 0, 'Selected color must be 0 or grater'),
+        assert(selectedColor < colors.length - 1,
+            'Selected color must be lower than ${colors.length}');
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colors[selectedColor]);
+}
