0d5632bbc7e25fcd5416855091cda6d87ef3d0a6
Luis Leal <luisrrleal@gmail.com>
2023-07-16 17:22:23 -0600

    Add router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -45,7 +46,9 @@ class _CustomListTile extends StatelessWidget {
         Icons.arrow_forward_ios_outlined,
         color: colors.primary,
       ),
-      onTap: (){},
+      onTap: () {
+        context.push(menuItem.url);
+      },
     );
   }
 }
