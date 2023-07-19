703e831e9f47051a74b5e98f6c4b2e6800387a8b
Luis Leal <luisrrleal@gmail.com>
2023-07-18 20:54:06 -0600

    Add new screens

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScroll extends StatelessWidget {
+  const InfiniteScroll({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
