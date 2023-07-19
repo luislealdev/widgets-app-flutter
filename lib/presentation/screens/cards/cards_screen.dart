1aaa9d59f2f32eff66ce08921e08740d4bac1fde
Luis Leal <luisrrleal@gmail.com>
2023-07-18 20:37:13 -0600

    Finished cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -32,7 +32,13 @@ class _CardsView extends StatelessWidget {
         child: Column(
       children: [
         ...cards.map((card) =>
-            CustomCard(elevation: card['elevation'], label: card['label']))
+            CustomCard(elevation: card['elevation'], label: card['label'])),
+        ...cards.map((card) => _BorderCustomCard(
+            elevation: card['elevation'], label: card['label'])),
+        ...cards.map((card) => _FilledBorderCustomCard(
+            elevation: card['elevation'], label: card['label'])),
+        ...cards.map((card) => _BorderImageCustomCard(
+            elevation: card['elevation'], label: card['label']))
       ],
     ));
   }
@@ -66,6 +72,121 @@ class CustomCard extends StatelessWidget {
           )
         ]),
       ),
+    );
+  }
+}
+
+class _BorderCustomCard extends StatelessWidget {
+  final double elevation;
+  final String label;
+
+  const _BorderCustomCard({
+    required this.elevation,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          side: BorderSide(color: colors.outline),
+          borderRadius: const BorderRadius.all(Radius.circular(20))),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.more_vert_outlined)),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - circular'),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class _FilledBorderCustomCard extends StatelessWidget {
+  final double elevation;
+  final String label;
+
+  const _FilledBorderCustomCard({
+    required this.elevation,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      shape: RoundedRectangleBorder(
+          side: BorderSide(color: colors.outline),
+          borderRadius: const BorderRadius.all(Radius.circular(20))),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.more_vert_outlined)),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - filled circular'),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class _BorderImageCustomCard extends StatelessWidget {
+  final double elevation;
+  final String label;
+
+  const _BorderImageCustomCard({
+    required this.elevation,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      color: colors.surfaceVariant,
+      shape: RoundedRectangleBorder(
+          side: BorderSide(color: colors.outline),
+          borderRadius: const BorderRadius.all(Radius.circular(20))),
+      elevation: elevation,
+      child: Stack(children: [
+        Image.network(
+          "https://picsum.photos/id/${elevation.toInt()}/600/350",
+          height: 350,
+          fit: BoxFit.cover,
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(20))),
+            child: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.more_vert_outlined)),
+          ),
+        ),
+      ]),
     );
   }
 }
