76cbed10cfdeeee6f8f570c2779b14733eaa1111
Luis Leal <luisrrleal@gmail.com>
2023-08-01 14:11:29 -0600

    Add drawer

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/widgets/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Widgets App')),
       body: const _HomeView(),
+      drawer: const SideMenu(),
     );
   }
 }
